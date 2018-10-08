67fd172f4b08b3c15af7a60403bd048b4500a3a6
Pawan Kumar <pk9910765616@gmail.com>
2018-10-08 10:20:02 +0530

    Begin Initial commit...

---
+++ app/src/main/java/com/example/hp/officialproject/activity_s/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hp.officialproject.activity_s
+
+import android.support.v7.app.AppCompatActivity
+
+
+/*
+created by PAWAN KUMAR
+*/
+
+
+class BaseActivity : AppCompatActivity() {
+}
