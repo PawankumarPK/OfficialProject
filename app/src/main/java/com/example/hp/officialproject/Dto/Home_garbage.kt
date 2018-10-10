f59a20ca619ffe66bb884e0f5bef404b1ca7fcaf
Pawan Kumar <pk9910765616@gmail.com>
2018-10-10 14:04:22 +0530

    add Recycler & adapter...

---
+++ app/src/main/java/com/example/hp/officialproject/Dto/Home_garbage.kt
@@ -0,0 +1,13 @@
+package com.example.hp.officialproject.Dto
+
+import android.graphics.Bitmap
+import android.widget.TextView
+
+
+/*
+created by PAWAN KUMAR
+*/
+
+
+data class Home_garbage(val dtoImage: Bitmap,val dtoTextviewForOff : String,val dtoTextViewItems:String ) {
+}
---
+++ app/src/main/java/com/example/hp/officialproject/adapter_s/RecyclerViewHolder.kt
@@ -0,0 +1,47 @@
+package com.example.hp.officialproject.adapter_s
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.hp.officialproject.Dto.Home_garbage
+import com.example.hp.officialproject.R
+import kotlinx.android.synthetic.main.home_garbage_view_holder.view.*
+
+
+/*
+created by PAWAN KUMAR
+*/
+
+
+class RecyclerViewHolder(private val context: Context,private val list: ArrayList<Home_garbage>) : RecyclerView.Adapter<RecyclerViewHolder.viewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder {
+      return viewHolder(LayoutInflater.from(context).inflate(R.layout.home_garbage_view_holder,parent,false))
+    }
+
+    override fun getItemCount(): Int {
+
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: viewHolder, position: Int) {
+
+        holder.bindItems(list[position])
+    }
+
+    inner class viewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+        fun bindItems(data : Home_garbage){
+
+            itemView.mHomeImageView.setImageBitmap(data.dtoImage)
+            itemView.mHomeOffTextview.text = data.dtoTextviewForOff
+            itemView.mHomeItemsDescTextView.text = data.dtoTextViewItems
+
+
+        }
+
+    }
+}
+
+//http://androidtechpoint.blogspot.com/2017/09/android-recycler-view-example-with-kotlin.html
