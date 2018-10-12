ab4078432e05e9210eae180cac6c9e15d55ffc54
Pawan Kumar <pk9910765616@gmail.com>
2018-10-12 14:06:16 +0530

    add Second Recyview items on recyclerview images and text using bitmap working forword...

---
+++ app/src/main/java/com/example/hp/officialproject/adapter_s/WasteGarbageViewAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.hp.officialproject.adapter_s
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.hp.officialproject.Dto.Dto_Home_garbage
+import com.example.hp.officialproject.R
+import kotlinx.android.synthetic.main.waste_garbage_view_holder.view.*
+
+
+/*
+created by PAWAN KUMAR
+*/
+
+
+class WasteGarbageViewAdapter(private val context: Context,private val list : ArrayList<Dto_Home_garbage>) : RecyclerView.Adapter<WasteGarbageViewAdapter.viewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder {
+        return viewHolder(LayoutInflater.from(context).inflate(R.layout.waste_garbage_view_holder,parent,false))
+
+    }
+
+    override fun getItemCount(): Int {
+
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: viewHolder, position: Int) {
+
+        holder.bindItems(list[position])
+    }
+
+    inner class viewHolder(view : View): RecyclerView.ViewHolder(view) {
+
+        fun bindItems(data : Dto_Home_garbage){
+
+            itemView.mWasteImageView.setImageBitmap(data.dtoImage)
+            itemView.mWasteOffTextview.text = data.dtoTextviewForOff
+            itemView.mWasteItemsTextView.text = data.dtoTextViewItems
+            itemView.mWasteItemsDescTextView.text = data.dtoTextviewDecription
+
+        }
+    }
+}
